cc962eace8849101e5a0330a581ed2821cea4eae
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2022-04-26 13:00:55 -0500

    update to training + addition to code for user model + super user

--- REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_api/models.py
+++ REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_api/models.py
@@ -1,6 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+# create profile manager
+class UserProfileManager(BaseUserManager):
+    """Manager for user priofiles.
+    Helps Django work with our custom user model."""
+
+    # create user with model
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile object."""
+
+        # logic to create a new user in the system
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        # normalize the email address - convert to lower case
+        email = self.normalize_email(email)
+
+        # create a new user profile object
+        user = self.model(email=email, name=name)
+
+        # set password - this encrypts the PW for us
+        # password is converted to a hash stored in the DB - no clear text
+        # therefore a None value will not allow for the user to be created
+        user.set_password(password)
+
+        # save using same DB used when we created the UserProfileManager
+        # Django can support multiple databases
+        user.save(using=self._db)
+
+        return user
+
+    # create super user
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True  # automatically created by PermissionsMixin
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
 
 # Create your models here.
 class UserProfile(AbstractBaseUser, PermissionsMixin):
