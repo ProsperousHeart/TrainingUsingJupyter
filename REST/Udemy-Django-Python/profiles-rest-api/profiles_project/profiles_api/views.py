0bd3b9bf53112237553d18b1644619eb302bbde2
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2022-04-26 19:18:08 -0500

    training update + code for adding POST method

--- REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_api/views.py
+++ REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_api/views.py
@@ -2,9 +2,15 @@
 from rest_framework.views import APIView
 # import Response class from rest_framework.response module
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
+
 
 class HelloAPIView(APIView):
     """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
 
     # API Views works by defining functions that match standard HTTP methods.
     def get(self, request, format=None):
@@ -20,3 +26,23 @@ class HelloAPIView(APIView):
         # return a Response instance
         #     Response({'message': '', 'variable': value, ...})
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        # create our serializer - pass in the request information
+        # serializer = serializers.HelloSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
+
+        # check if serializer has valid data
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            # return a message about what went wrong and the status
+            # 400 means bad request to API
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                )
