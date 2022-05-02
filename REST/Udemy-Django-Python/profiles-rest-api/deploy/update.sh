f12fe30a48e4354fde8256352538710d46013cb5
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2022-05-01 22:19:11 -0500

    Configured for deployment API training

---
+++ REST/Udemy-Django-Python/profiles-rest-api/deploy/update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+# PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/django-rest-proj1'
+
+git pull
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+supervisorctl restart profiles_api
+
+echo "DONE! :)"
--- REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_project/settings.py
+++ REST/Udemy-Django-Python/profiles-rest-api/profiles_project/profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^q5$0es%wzg@beiovt5z$627452lu5@$7)f3aqat&1!ru*)fu$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"  # app_name.model
+
+STATIC_ROOT = 'static/'
